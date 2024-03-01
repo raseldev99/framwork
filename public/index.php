876640cc8a019791466fb292cbc5d1249f79c656
Rasel Ahmed <raselbabu34.bd@gmail.com>
2024-03-01 19:37:04 +0600

    update some problem

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 	require_once __DIR__.'/../vendor/autoload.php';
+	
+	use App\Config\Config;
 	use App\Controllers\Controller;
 	use App\Route;
+	
+    Config::run();
 	Route::get('/test',[Controller::class,'index']);
 	Route::run();
